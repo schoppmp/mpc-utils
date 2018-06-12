3682462fa4cd33bc0f2646ec1d21eb518deeb0c1
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-06-12 19:10:32 +0200

    Add serialization support for Eigen::SparseMatrix

--- boost_serialization/eigen.hpp
+++ boost_serialization/eigen.hpp
@@ -2,9 +2,10 @@
 
 #include <boost/serialization/vector.hpp>
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 namespace boost { namespace serialization {
-
+// Dense matrices
 // From Stackoverflow by Shmuel Levine, Licensed CC-BY-SA 3.0
 // https://stackoverflow.com/a/22903063
 template<   class Archive,
@@ -60,5 +61,58 @@ inline void serialize(
 {
     split_free(ar, g, version);
 }
+
+// sparse matrices
+template<typename Archive, typename T, typename Index>
+inline void serialize(
+	Archive &ar,
+	Eigen::Triplet<T, Index>& triplet,
+	const unsigned int version
+) {
+	Index i = triplet.row(), j = triplet.col();
+	T val = triplet.value();
+	ar & i; ar & j; ar & val;
+	triplet = Eigen::Triplet<T, Index>(i, j, val);
+}
+
+template<typename Archive, typename T, int Ops_, typename StorageIndex_>
+inline void save(
+	Archive& ar,
+	const Eigen::SparseMatrix<T, Ops_, StorageIndex_>& mat,
+	const unsigned int version
+) {
+	// TODO: maybe there's a way to save the intermediate copy?
+	std::vector<Eigen::Triplet<T, StorageIndex_>> triplets;
+	for(size_t k=0; k<mat.outerSize(); k++) {
+	  for(typename Eigen::SparseMatrix<T, Ops_, StorageIndex_>::InnerIterator
+			it(mat,k); it; ++it
+		) {
+			triplets.push_back(Eigen::Triplet<T, StorageIndex_>(it.row(), it.col(),
+				it.value()));
+	  }
+	}
+	ar & triplets;
+}
+
+template<typename Archive, typename T, int Ops_, typename StorageIndex_>
+inline void load(
+	Archive& ar,
+	Eigen::SparseMatrix<T, Ops_, StorageIndex_>& mat,
+	const unsigned int version
+) {
+	std::vector<Eigen::Triplet<T, StorageIndex_>> triplets;
+	ar & triplets;
+	mat.setFromTriplets(triplets.begin(), triplets.end());
+}
+
+template<typename Archive, typename T, int Ops_, typename StorageIndex_>
+inline void serialize(
+	Archive& ar,
+	Eigen::SparseMatrix<T, Ops_, StorageIndex_>& mat,
+	const unsigned int version
+) {
+	split_free(ar, mat, version);
+}
+
 
 }} // namespace boost::serialization
