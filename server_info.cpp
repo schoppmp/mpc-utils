9336f297abad69b9db9f0d2b4808c585e194a565
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-11-29 15:01:49 +0100

    Format server_info.{h,c}pp

--- server_info.cpp
+++ server_info.cpp
@@ -1,7 +1,7 @@
 #include "server_info.hpp"
 #include <ostream>
 
-int server_info::compare(const server_info &other) const {
+int server_info::compare(const server_info& other) const {
   int ret = host.compare(other.host);
   if (!ret) {  // hostnames equal
     if (port < other.port) return -1;
@@ -11,6 +11,6 @@ int server_info::compare(const server_info &other) const {
   return ret;
 }
 
-std::ostream &operator<<(std::ostream &stream, const server_info &server) {
+std::ostream& operator<<(std::ostream& stream, const server_info& server) {
   return stream << "Server host: " << server.host << ", port: " << server.port;
 }
--- server_info.hpp
+++ server_info.hpp
@@ -5,27 +5,27 @@ struct server_info {
   std::string host;
   uint16_t port;
 
-  server_info(std::string &host, uint16_t port) : host(host), port(port) {}
+  server_info(std::string& host, uint16_t port) : host(host), port(port) {}
 
-  int compare(const server_info &other) const;
-  bool operator==(const server_info &other) const {
+  int compare(const server_info& other) const;
+  bool operator==(const server_info& other) const {
     return (compare(other) == 0);
   };
-  bool operator!=(const server_info &other) const {
+  bool operator!=(const server_info& other) const {
     return (compare(other) != 0);
   };
-  bool operator<=(const server_info &other) const {
+  bool operator<=(const server_info& other) const {
     return (compare(other) <= 0);
   };
-  bool operator>=(const server_info &other) const {
+  bool operator>=(const server_info& other) const {
     return (compare(other) >= 0);
   };
-  bool operator>(const server_info &other) const {
+  bool operator>(const server_info& other) const {
     return (compare(other) > 0);
   };
-  bool operator<(const server_info &other) const {
+  bool operator<(const server_info& other) const {
     return (compare(other) < 0);
   };
 };
 
-std::ostream &operator<<(std::ostream &stream, const server_info &server);
+std::ostream& operator<<(std::ostream& stream, const server_info& server);
